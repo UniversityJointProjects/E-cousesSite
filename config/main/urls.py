d0c841beada5c4dc5adaa1003d4ee537a2defbf3
Evgeny Zimin <nullptr>
2023-02-10 22:15:42 +0400

    added edit profile design

--- config/main/urls.py
+++ config/main/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.introduce, name="introduce"),
     path('introduce', views.introduce, name="introduce"),
+    path('announcements', views.announcements, name="announcements"),
     path('table_show/<int:tk>', views.table_view, name='table_show'),
     path('change_table/<int:url_table_id>/<int:entry_id>/<command>', views.change_table, name='change_table'),
     path('registration', views.registration, name='registration'),
